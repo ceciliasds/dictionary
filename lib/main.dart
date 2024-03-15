4ff2687b615128e6da3b7d82098d7d3db4505660
jimxdxd <144504691+Jimwelljanxdxd@users.noreply.github.com>
2024-03-15 13:35:04 +0800

    Delos Santos, Cecilia

--- lib/main.dart
+++ lib/main.dart
@@ -25,3 +25,57 @@ class _DictionaryScreenState extends State<DictionaryScreen> {
     super.initState();
     _fetchWordOfTheDay();
   }
+
+  Future<void> _fetchWordOfTheDay() async {
+    final response =
+    await http.get(Uri.parse("https://api.dictionaryapi.dev/api/v2/entries/en/random"));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+      final wordOfTheDay = data[0]['word'];
+      await _fetchDefinition(wordOfTheDay);
+    } else {
+      setState(() {
+        _word = "";
+        _searchedWord = "Failed to fetch the Word of the Day";
+        _definitions = [];
+      });
+    }
+  }
+
+  Future<void> _fetchRandomWord() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    DateTime? lastFetchedDate =
+    DateTime.tryParse(prefs.getString('lastFetchedDate') ?? '');
+
+    if (lastFetchedDate == null ||
+        !isSameDay(lastFetchedDate, DateTime.now())) {
+      final response = await http
+          .get(Uri.parse("https://random-word-api.herokuapp.com/word?number=1"));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+        final randomWord = data[0];
+        await _fetchDefinition(randomWord);
+        prefs.setString('lastFetchedDate', DateTime.now().toString());
+      } else {
+        setState(() {
+          _word = "";
+          _searchedWord = "Failed to fetch random word";
+          _definitions = [];
+        });
+      }
+    } else {
+      setState(() {
+        _word = "";
+        _searchedWord = "Word of the Day already fetched today";
+        _definitions = [];
+      });
+    }
+  }
+
+  bool isSameDay(DateTime date1, DateTime date2) {
+    return date1.year == date2.year &&
+        date1.month == date2.month &&
+        date1.day == date2.day;
+  }
